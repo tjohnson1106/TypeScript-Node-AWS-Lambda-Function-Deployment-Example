3afcba1d909e35cdeb39876a8f0ce61a2fb76368
Thomas Johnson <tjohnson1106@gmail.com>
2017-08-27 20:37:15 -0400

    added types to each of the function's aparameters

---
+++ handlers.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+import { Handler, Context, Callback } from 'aws-lambda';
+
+interface HelloResponse {
+  statusCode: number;
+  body: string;
+}
+
+const hello: Handler = (event: any, context: Context, callback: Callback) => {
+  const response: HelloResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: Math.floor(Math.random() * 10)
+    })
+  };
+
+  callback(undefined, response);
+   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+export { hello }
